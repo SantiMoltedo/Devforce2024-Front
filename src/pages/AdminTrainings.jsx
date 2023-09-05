32c46278f950fbc2411877b238ac490746467a0f
MRAGUADO <mbra2004@gmail.com>
2023-09-05 18:35:03 -0300

    LIC-00014 Fix strings by adding a space

--- src/pages/AdminTrainings.jsx
+++ src/pages/AdminTrainings.jsx
@@ -255,7 +255,7 @@ export const AdminTrainings = () => {
                                       </a>
                                     )}
                                     <span>
-                                      {soli.userId.firstname +
+                                      {soli.userId.firstname + ' ' +
                                         soli.userId.lastname}
                                     </span>
                                   </div>
@@ -279,7 +279,7 @@ export const AdminTrainings = () => {
                                       </a>
                                     )}
                                     <span>
-                                      {soli.mentorId.firstname +
+                                      {soli.mentorId.firstname + ' ' +
                                         soli.mentorId.lastname}
                                     </span>
                                   </div>
